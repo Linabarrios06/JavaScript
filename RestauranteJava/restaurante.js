ac35ac72ee3655f4a6a39dcb0efd4b896f45c298
Linabarrios06 <129072393+Linabarrios06@users.noreply.github.com>
2023-11-21 13:56:15 -0500

    comenarios

--- RestauranteJava/restaurante.js
+++ RestauranteJava/restaurante.js
@@ -1,12 +1,12 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => {  //Funccion flecha -addEventListener Registra un evento a un objeto en específico
     const orderForm = document.getElementById("order-form");
-    const orderSummary = document.getElementById("order-summary");
+    const orderSummary = document.getElementById("order-summary"); //getElementById seleccionar un elemento del documento por medio del valor del atributo id que se le haya asignado.
     const totalPrice = document.getElementById("total-price");
 
     let total = 0;
 
     orderForm.addEventListener("submit", (event) => {
-        event.preventDefault();
+        event.preventDefault(); //event  cosas que pasan en el sistema que estás programando, el cual se encarga de avisarte para que tu código pueda hacer algo al respecto
     });
 
     document.getElementById("add-to-order").addEventListener("click", () => {
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", () => {
         const drink = document.getElementById("select-drink").value;
         const drinkQuantity = parseInt(document.getElementById("drink-quantity").value);
 
-        const dishPrices = {
+        const dishPrices = { //Creamos  las opciones del menu
             "cazuela de mariscos personal": 32000,
             "pescado frito personal": 25000,
             "coctel de camarones personal": 20000,
@@ -40,7 +40,7 @@ document.addEventListener("DOMContentLoaded", () => {
             "camarones grande": 27000,
         };
 
-        const drinkPrices = {
+        const drinkPrices = { //Creamos las opciones del menu
             "agua personal": 3000,
             "limonada personal": 4000,
             "piña colada personal": 10000,
@@ -57,18 +57,19 @@ document.addEventListener("DOMContentLoaded", () => {
             "mojito grande": 18000,
         };
 
-        const dishPrice = dishPrices[dish] || 0;
+        const dishPrice = dishPrices[dish] || 0; //todos los valores empizan en cero
         const drinkPrice = drinkPrices[drink] || 0;
 
-        const subtotal = dishPrice * dishQuantity + drinkPrice * drinkQuantity;
+        const subtotal = dishPrice * dishQuantity + drinkPrice * drinkQuantity; //
 
         const orderItem = document.createElement("div");
-        orderItem.classList.add("order-item");
+        //add permite añadir un elemento al conjunto
+        orderItem.classList.add("order-item"); //classList  forma práctica de acceder a la lista de clases de un elemento como una cadena de texto delimitada por espacios a través de element.
         orderItem.innerHTML = `Plato: ${dish} (x${dishQuantity}), Bebida: ${drink} (x${drinkQuantity})`;
 
-        orderSummary.appendChild(orderItem);
-
+        orderSummary.appendChild(orderItem); //appendChild  agregar nuevos elementos a un documento existente o para mover un elemento de la página
+ 
         total += subtotal;
-        totalPrice.textContent = total.toFixed();
+        totalPrice.textContent = total.toFixed(); //toFixed devuelve una representación de cadena de numObj que no usa notación exponencial y tiene exactamente dígitos dígitos después del decimal
     });
 });
